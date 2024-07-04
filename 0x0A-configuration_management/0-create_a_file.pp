1f60ecd1c8dcaa7c0a8bbcb1ffad2610e8c989c5
harystyles <harystyleseze@gmail.com>
2024-07-03 17:22:37 -0700

    config management

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This manifest creates a file at /tmp/school with specific permissions, owner, group, and content.
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
